ecbae3c16ea94359115c545ca5ef4a7d96ee4a12
PottierLoic <loic.pottier@live.fr>
2023-04-21 00:41:41 +0200

    Update cpu.v

--- src/cpu.v
+++ src/cpu.v
@@ -7,9 +7,9 @@ mut:
 
 fn (mut cpu Cpu) execute(instruction Instruction, target ArithmeticTarget) {
 	match instruction {
-		add {
+		.add {
 			match target {
-				c {
+				.c {
 					mut value := cpu.registers.c
 					mut new_value := cpu.add(value)
 					cpu.registers.a = new_value
@@ -25,7 +25,15 @@ fn (mut cpu Cpu) execute(instruction Instruction, target ArithmeticTarget) {
 	}
 }
 
-fn (cpu Cpu) add(value u8) u8 {
-	print("add")
-	return u8(1)
+fn (mut cpu Cpu) add(value u8) u8 {
+	// must replace overflowing_add that works in rust with a v equivalent
+	new_value, did_overflow := cpu.registers.a.overflowing_add(value)
+	flags := FlagsRegister{
+		zero: new_value == 0
+		subtract: false
+		half_carry: (cpu.registers.a & 0xf) + (value & 0xf) > 0xf
+		carry: did_overflow
+	}
+	cpu.registers.f = flags
+	return new_value
 }
