11ef0a1a8a3c0cdb4c97c45326c243c5dab71f75
PottierLoic <loic.pottier@live.fr>
2023-08-29 17:57:36 +0200

    fetch started

--- src/cpu.v
+++ src/cpu.v
@@ -9,40 +9,85 @@ pub mut:
 	ie_register        u8
 	interruption_flags u8
 	int_master_enabled bool
-	ime_enabled 			 bool
+	ime_enabled        bool
 
-	func							 [34]fn()
+	func [34]fn ()
 
-	fetched_opcode u8
+	/* Step processing */
+	fetched_opcode      u8
 	fetched_instruction Instruction
-	fetched_data u16
+	fetched_data        u16
+	destination         u16
+	memory              bool
 }
 
+/* Get the opcode at PC and retrieve the corresponding instruction */
 pub fn (mut cpu Cpu) fetch_instruction() {
 	cpu.fetched_opcode = cpu.read_byte(cpu.registers.pc)
 	cpu.fetched_instruction = instruction_from_byte(cpu.fetched_opcode)
 }
 
+/* Get datas depending on the fetched instruction addressing mode */
 pub fn (mut cpu Cpu) fetch_data() {
 	match cpu.fetched_instruction.mode {
 		.am_imp {}
-		.am_r { cpu.fetched_data = cpu.get_reg(cpu.fetched_instruction.reg_1) }
-		.am_r_r { cpu.fetched_data = cpu.get_reg(cpu.fetched_instruction.reg_2) }
+		.am_r {
+			cpu.fetched_data = cpu.get_reg(cpu.fetched_instruction.reg_1)
+		}
+		.am_r_r {
+			cpu.fetched_data = cpu.get_reg(cpu.fetched_instruction.reg_2)
+		}
 		.am_r_d8 {
 			cpu.fetched_data = cpu.read_byte(cpu.registers.pc)
 			cpu.vboy.timer_cycle(1)
 			cpu.registers.pc++
 		}
 		.am_r_d16, .am_d16 {
-			u16 lower__byte := cpu.read_byte(cpu.registers.pc)
+			lower__byte := cpu.read_byte(cpu.registers.pc)
 			cpu.vboy.timer_cycle(1)
-			u16 higher_byte := cpu.read_byte(cpu.registers.pc + 1)
+			higher_byte := cpu.read_byte(cpu.registers.pc + 1)
 			cpu.vboy.timer_cycle(1)
 			cpu.fetched_data = lower__byte | (higher_byte << 8)
 			cpu.registers.pc += 2
 		}
-		.
-		else {}
+		.am_mr_r {
+			cpu.fetched_data = cpu.get_reg(cpu.fetched_instruction.reg_2)
+			cpu.destination = cpu.get_reg(cpu.fetched_instruction.reg_1)
+			cpu.memory = true
+			if cpu.fetched_instruction.reg_1 == .reg_c {
+				cpu.destination |= 0xFF00
+			}
+		}
+		.am_r_mr {
+			mut destination := cpu.get_reg(cpu.fetched_instruction.reg_2)
+			if cpu.fetched_instruction.reg_2 == .reg_c {
+				destination |= 0xFF00
+			}
+			cpu.vboy.timer_cycle(1)
+			cpu.fetched_data = cpu.read_byte(destination)
+		}
+		.am_r_hli {
+			cpu.fetched_data = cpu.vboy.read_byte(cpu.get_reg(fetched_instruction.reg_2))
+			cpu.vboy.timer_cycle(1)
+			cpu.registers.set_hl(cpu.get_reg(.reg_hl) + 1)
+		}
+		.am_r_hld {}
+		.am_hli_r {}
+		.am_hld_r {}
+		.am_r_a8 {}
+		.am_a8_r {}
+		.am_hl_spr {}
+		.am_d8 {}
+		.am_a16_r, .am_d16_r {}
+		.am_mr_d8 {}
+		.am_mr {}
+		.am_r_a16 {}
+		.am_none {
+			panic('am_none, this should never happend.')
+		}
+		else {
+			panic('Unknown addressing mode: ${cpu.fetched_instruction.mode}')
+		}
 	}
 }
 
@@ -158,11 +203,19 @@ pub fn (mut cpu Cpu) set_interruption_flags(value u8) {
 	cpu.interruption_flags = value
 }
 
-pub fn (mut cpu Cpu) set_flags(z int, n int, h int, c int){
-	if z != -1 { bit_set(cpu.registers.f, zero_flag_byte_position, z)}
-	if n != -1 { bit_set(cpu.registers.f, subtract_flag_byte_position, n)}
-	if h != -1 { bit_set(cpu.registers.f, half_carry_flag_byte_position, h)}
-	if c != -1 { bit_set(cpu.registers.f, carry_flag_byte_position, c)}
+pub fn (mut cpu Cpu) set_flags(z int, n int, h int, c int) {
+	if z != -1 {
+		bit_set(cpu.registers.f, zero_flag_byte_position, z)
+	}
+	if n != -1 {
+		bit_set(cpu.registers.f, subtract_flag_byte_position, n)
+	}
+	if h != -1 {
+		bit_set(cpu.registers.f, half_carry_flag_byte_position, h)
+	}
+	if c != -1 {
+		bit_set(cpu.registers.f, carry_flag_byte_position, c)
+	}
 }
 
 fn (mut cpu Cpu) get_reg(reg Reg) u16 {
@@ -181,6 +234,6 @@ fn (mut cpu Cpu) get_reg(reg Reg) u16 {
 		.reg_hl { cpu.registers.get_hl() }
 		.reg_pc { cpu.registers.pc }
 		.reg_sp { cpu.registers.sp }
-		.reg_none { panic("Reg none should never happend, unless..") }
+		.reg_none { panic('Reg none should never happend, unless..') }
 	}
 }
